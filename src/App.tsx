05d884674a367acfca688f25cf7665dc99d00d80
levnet <abc@gmail.com>
2021-12-25 16:43:06 +1100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import HomePage from "./pages/HomePage";
+import MintPage from "./components/MintPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/mint" component={MintPage} />
+                </Switch>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
